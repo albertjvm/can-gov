da2e095fa84e786384f2f171cf2298cd6138d39e
Albert VanderMeulen <albert.vandermeulen@centro-201972.centro.net>
2021-09-11 16:24:28 -0400

    link to mp details from reps view

--- src/views/Representatives/Representatives.js
+++ src/views/Representatives/Representatives.js
@@ -1,10 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { searchPostalCode } from '../../api';
 import { Grid } from '../../components';
 import './Representatives.scss';
 
 export const Representatives = () => {
+    const history = useHistory();
     const { postalCode } = useParams();
     const [representatives, setRepresentatives] = useState([]);
     const [city, setCity] = useState('');
@@ -21,13 +22,34 @@ export const Representatives = () => {
         }));
     }, [adjustedPostalCode]);
 
+    const nameRenderer = (name, { elected_office }) => {
+        if (elected_office !== "MP") return name;
+
+        const id = name
+            .toLowerCase()
+            .normalize("NFD").replace(/\p{Diacritic}/gu, "")
+            .replace(/[.']/g, '')
+            .split(' ').join('-');
+            
+        return <a href={`/mps/${id}`}>{name}</a>
+    };
+
+    const sortedData = () => (
+        representatives.sort((r1, r2) => {
+            if (r1?.elected_office === 'MP') return -1;
+            if (r2?.elected_office === 'MP') return 1;
+            return 0;
+        })
+    );
+
     return (
         <section className="Representatives">
             <h2>{adjustedPostalCode()} - {city}, {province}</h2>
             <Grid
-                data={representatives}
+                data={sortedData()}
+                rowClassnameFn={({ elected_office }) => elected_office.toLowerCase()}
                 columns={[
-                    { name: 'Name', dataKey: "name", flexWeight: 2 },
+                    { name: 'Name', dataKey: "name", flexWeight: 2, renderer: nameRenderer },
                     { name: 'Title', dataKey: "elected_office" },
                     { name: 'District', dataKey: "district_name" },
                     { name: 'Email', dataKey: "email", flexWeight: 2, renderer: (d) => (
